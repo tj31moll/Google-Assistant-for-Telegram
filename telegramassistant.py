4f40919a5562dfc40d08b797db8979104cc5c7b9
tj31moll <91799649+tj31moll@users.noreply.github.com>
2023-03-21 20:39:41 -0400

    Update telegramassistant.py

--- telegramassistant.py
+++ telegramassistant.py
@@ -32,24 +32,13 @@ from telegram.error import Unauthorized
 from telegram.ext import Filters
 from telegram.ext import MessageHandler
 from telegram.ext import Updater
+from config import ASSISTANT_API_ENDPOINT, DEFAULT_GRPC_DEADLINE, BOT_TOKEN, ALLOWED_CHAT_IDS, AUTHORIZED_USER_IDS, DEVICE_MODEL_ID, DEVICE_ID
+
 
 try:
     from . import assistant_helpers
 except (SystemError, ImportError):
     import assistant_helpers
-
-
-ASSISTANT_API_ENDPOINT = 'embeddedassistant.googleapis.com'
-DEFAULT_GRPC_DEADLINE = 60 * 3 + 5
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
-ALLOWED_CHAT_IDS = list(
-        map(int, os.environ.get('ALLOWED_CHAT_IDS').split(','))
-        )
-AUTHORIZED_USER_IDS = list(
-        map(int, os.environ.get('AUTHORIZED_USER_IDS').split(','))
-        )
-DEVICE_MODEL_ID = os.environ.get('DEVICE_MODEL_ID')
-DEVICE_ID = os.environ.get('DEVICE_ID')
 
 
 class SampleTextAssistant(object):
